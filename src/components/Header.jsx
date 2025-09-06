9ae233ffbfaa3f25ed55a03d60e254ab7e1d3cd5
shreyaanjan <shreyaanjan02@gmail.com>
2025-09-06 22:56:25 +0530

    minor design error solved

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,24 +12,26 @@ const Header = () => {
                     <Link to={"/"} className="flex items-center space-x-3 rtl:space-x-reverse">
                         <img src="/images/logo.png" alt="logo" />
                     </Link>
-                    <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-                        <ul className="font-medium flex flex-col p-4 md:flex-row md:space-x-8 rtl:space-x-reverse">
-                            <li>
-                                <Link to={"/"} className={`${pathname == "/" ? "text-[#E27614]" : "text-white"}`}>Home</Link>
-                            </li>
-                            <li>
-                                <Link to={"/add-movie"} className={`${pathname == "/add-movie" ? "text-[#E27614]" : "text-white"}`}>Add Movie</Link>
-                            </li>
-                            <li>
-                                <Link to={"/display-movie"} className={`${pathname == "/display-movie" || pathname.includes("/description") ? "text-[#E27614]" : "text-white"}`}>Display Movie</Link>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className="flex items-center gap-5">
-                        <div className="md:hidden">
-                            <button onClick={() => setMenu(!menu)} className="text-white text-2xl">
-                                {menu ? '✕' : '☰'}
-                            </button>
+                    <div className="flex items-center justify-between">
+                        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+                            <ul className="font-medium flex flex-col p-4 md:flex-row md:space-x-8 rtl:space-x-reverse">
+                                <li>
+                                    <Link to={"/"} className={`${pathname == "/" ? "text-[#E27614]" : "text-white"}`}>Home</Link>
+                                </li>
+                                <li>
+                                    <Link to={"/add-movie"} className={`${pathname == "/add-movie" ? "text-[#E27614]" : "text-white"}`}>Add Movie</Link>
+                                </li>
+                                <li>
+                                    <Link to={"/display-movie"} className={`${pathname == "/display-movie" || pathname.includes("/description") ? "text-[#E27614]" : "text-white"}`}>Display Movie</Link>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="flex items-center gap-5">
+                            <div className="md:hidden">
+                                <button onClick={() => setMenu(!menu)} className="text-white text-2xl">
+                                    {menu ? '✕' : '☰'}
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -38,13 +40,13 @@ const Header = () => {
                         <div className="md:hidden fixed top-[72px] left-0 right-0 bottom-0 bg-[#1B2335] z-50 p-6 overflow-y-auto">
                             <ul className="flex flex-col space-y-4 p-2 font-semibold">
                                 <li>
-                                    <Link to={"/"}  onClick={() => setMenu(false)}  className={`${pathname == "/" ? "text-[#E27614]" : "text-white"}`}>Home</Link>
+                                    <Link to={"/"} onClick={() => setMenu(false)} className={`${pathname == "/" ? "text-[#E27614]" : "text-white"}`}>Home</Link>
                                 </li>
                                 <li>
-                                    <Link to={"/add-movie"}  onClick={() => setMenu(false)}  className={`${pathname == "/add-movie" ? "text-[#E27614]" : "text-white"}`}>Add Movie</Link>
+                                    <Link to={"/add-movie"} onClick={() => setMenu(false)} className={`${pathname == "/add-movie" ? "text-[#E27614]" : "text-white"}`}>Add Movie</Link>
                                 </li>
                                 <li>
-                                    <Link to={"/display-movie"}  onClick={() => setMenu(false)}  className={`${pathname == "/display-movie" || pathname.includes("/description") ? "text-[#E27614]" : "text-white"}`}>Display Movie</Link>
+                                    <Link to={"/display-movie"} onClick={() => setMenu(false)} className={`${pathname == "/display-movie" || pathname.includes("/description") ? "text-[#E27614]" : "text-white"}`}>Display Movie</Link>
                                 </li>
                             </ul>
                         </div>
